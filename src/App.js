2298574865f514551272acd04e0ec9037092507e
hollowgxd <devdplayer@yandex.ru>
2024-12-12 22:46:10 +0300

    spo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from "./header/Header";
+import React from "react";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import Search from "./components/search/Search";
+import "./App.css";
 
 function App() {
-    function PenisSwitch(penisSize)
-    {
-        if (penisSize === 1){
-            console.log("понятно");
-        return}
-        if (penisSize === 0){
-            console.log("буээ");
-        return;}
-    }
-  return (
-      <div className="App">
-          <Header/>
-        <h1 id="envelope"> ENVELOUPEgg </h1>
-        <h2> какая у сани</h2>
-        <button className="Popa" onClick={PenisSwitch(1)}> @=============3</button>
-        <button className="Popa" onClick={PenisSwitch(0)}> @==3</button>
-          <div className="container">
-              <div className="div1"></div>
-              <div className="div2"></div>
-              <div className="div3"></div>
-
-          </div>
-      </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="hero container">
+                <Hero />
+            </div>
+            <Search />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="container">
+            <span className="logo">w8what</span>
+            <nav>
+                <ul>
+                    <li className="active"><a href="#">temp</a></li>
+                    <li className="btn"><a href="#">Какая вакансия вас интересует</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
